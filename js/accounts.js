b6b6fc64bc902725fc5439bf2978f99783fddf25
Ciprian <56521866+caciuleanuciprian@users.noreply.github.com>
2020-09-01 13:24:13 +0300

    fixed login and redirecting

--- js/accounts.js
+++ js/accounts.js
@@ -1,3 +1,5 @@
+// Small accounts database
+
 const accounts = [
 
     {
@@ -7,21 +9,25 @@ const accounts = [
     {
         username: "andrei",
         password: "parola"
+    },
+    {
+        username: "123",
+        password: "123"
     }
 
 ];
 
-// Not working
+// Checking if the account exists    
 
 function checkInfo(){
-    event.preventDefault()
-    const username = document.getElementById(username).value;
-    const password = document.getElementById(password).value;
-    for(let i = 0; i < accounts.length; i++){
+    event.preventDefault()                                                              // prevents url changing
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    for(let i = 0; i < accounts.length; i++){                                           // looping through the array "accounts"
         if(username == accounts[i].username && password == accounts[i].password){
-            window.location.replace("http://www.google.com");
-        }else{
-            console.log("nu merge");
+            window.location.replace("loading.html");                                    // redirect to loading screen
+            return;
         }
-    }
+    }  
+        alert("Username or password is incorrect. Please try again.");                  // error message
 }
