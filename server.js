e96258dd17c381d76e832e704f8831154f9d33f9
Caciuleanu Ciprian <caciuleanu.ciprian@gmail.com>
2020-09-09 17:02:16 +0300

    a

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const port = 3000
+
+const server = http.createServer(function(req, res){
+  
+  
+})
+
+server.listen(port, function(error){
+  if(error){
+    console.log('Something went wrong', error)
+  }else{
+    console.log('Server is listening on port ' + port)
+  }
+})
+
+const io = require('socket.io')(3000);
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+    })
+    socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+    socket.emit('chat-message', { message: message, name: users[socket.id] });
+    })
+    socket.on('disconnect', name => {
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+    })
+})
