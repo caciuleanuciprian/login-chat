ca184c3df7ceccf949f2a08421ed79bbf1280582
Caciuleanu Ciprian <caciuleanu.ciprian@gmail.com>
2020-09-16 15:57:41 +0300

    backedup

--- server.js
+++ server.js
@@ -1,28 +1,3 @@
-const http = require('http')
-const fs = require('fs')
-const port = process.env.PORT || 8000;
-
-const server = http.createServer(function(req, res){
-    res.writeHead(200, { 'Content-Type': ''})
-    fs.readFile('./', function(error, data){
-      if(error){
-        res.writeHead(404)
-        res.write('Error:File Not Found')
-      }else{
-        res.write(data)
-      }
-      res.end();
-    })
-})
-
-server.listen(port, function(error){
-  if(error){
-    console.log('Something went wrong', error)
-  }else{
-    console.log('Server is listening on port ' + port)
-  }
-})
-
 const io = require('socket.io')(3000);
 
 const users = {};
