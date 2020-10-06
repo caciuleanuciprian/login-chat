b0ff2e12e9350393c5f2bf83828a3912a0632340
Caciuleanu Ciprian <caciuleanu.ciprian@gmail.com>
2020-10-06 13:37:37 +0300

    d

--- accounts.js
+++ accounts.js
@@ -26,6 +26,7 @@ function checkInfo(){
     const password = document.getElementById("password").value;
     for(let i = 0; i < accounts.length; i++){                                           // looping through the array "accounts"
         if(username == accounts[i].username && password == accounts[i].password){
+            localStorage.setItem(1, accounts[i].username);
             window.location.replace("loading.html");                                    // redirect to loading screen
             return;
             
